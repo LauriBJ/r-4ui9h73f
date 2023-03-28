f6c6f21dc764a2dda0ad80ffe5e170ad60ee31c2
Laura V. Buttner J <laurita.buttner@gmail.com>
2023-03-27 20:50:35 -0400

    al fin me salio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={e => this.setState({ text: e.target.value })} 
+         value={this.state.text}
+        />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
